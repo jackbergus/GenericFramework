579a31671acbe01a47276571e852756007ad7edc
Bergami Giacomo, PhD <bergamigiacomo@gmail.com>
2026-03-26 22:24:29 +0100

    Adding appropriate licence information

--- include/jackbergus/data/NetworkFloat.h
+++ include/jackbergus/data/NetworkFloat.h
@@ -1,6 +1,20 @@
+// NetworkFloat.h
+// This file is part of GeneralFramework
 //
-// Created by gyankos on 11/03/26.
+// Copyright (C)  2026 - Giacomo Bergami
 //
+// GeneralFramework is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  GeneralFramework is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with GeneralFramework. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef FLOATMAX_NETWORKFLOAT_H
 #define FLOATMAX_NETWORKFLOAT_H
--- include/jackbergus/framework/View.h
+++ include/jackbergus/framework/View.h
@@ -1,20 +1,20 @@
 // View.h
 // This file is part of GeneralFramework
 //
-// Copyright (C)  2026 - gyankos
+// Copyright (C)  2026 - Giacomo Bergami
 //
-// narcissus is free software; you can redistribute it and/or modify
+// GeneralFramework is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
 //  (at your option) any later version.
 //
-//  narcissus is distributed in the hope that it will be useful,
+//  GeneralFramework is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with narcissus. If not, see <http://www.gnu.org/licenses/>.
+//  along with GeneralFramework. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef GENERALFRAMEWORK_VIEW_H
 #define GENERALFRAMEWORK_VIEW_H
